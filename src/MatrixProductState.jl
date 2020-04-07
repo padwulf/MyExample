2cc2c9fcb6a60eb8dceb01e5c408e71145e44907
padwulf <pieter.dewulf@ugent.be>
2020-04-07 13:51:24 +0200

    precompute and store tensornetwork in Mps

--- src/MatrixProductState.jl
+++ src/MatrixProductState.jl
@@ -2,15 +2,38 @@ import Base.*
 using TensorOperations
 
 struct Mps{}
-    matrices::Array{AbstractMatrix}
-    optimalOrder::Vector{Int}
+    matrices_                #
+    contractions_
+    contractionorder_
     function Mps(A::AbstractMatrix,B::AbstractMatrix...)
-        return new([A,B...], zeros(length([A,B...])))
+        M = [A,B...]
+        Mps(M)
     end
     function Mps(M)
-        return new(M, zeros(length(M)))
+        #contractionorder
+        order = optimalSequence1(size.(M))
+        #contractions
+        contractions = []
+        for i in 1:length(M)                   # matrices' connections to the network
+            push!(contractions, [-i, i])
+        end
+        push!(contractions, collect(1:length(M)))   # de tensors conectons
+        # tensors
+        tensors = Array{Array,1}(undef, length(M))
+        tensors[1:end]=M
+
+        return new(tensors, contractions, order)
     end
 end
+
+function optimalmul2(mps::Mps, T::AbstractArray)
+    push!(mps.matrices_, T)
+    res = ncon(mps.matrices_, mps.contractions_, order = mps.contractionorder_)
+    pop!(mps.matrices_)
+    return res
+end
+
+
 
 function optimalSequence1(array)
     #voor het algemene geval, brute force.
@@ -19,8 +42,8 @@ function optimalSequence1(array)
     (optcost, optseq) =  costPermutatingrecursive([], array, product, 0, Inf, array)
     return [t[3] for t in optseq]                                                   #return only indexnumbers of permutation
 end
-setOptimalOrder(mps::Mps, o::Vector{Int}) = (mps.optimalOrder[1:end]=o)
 
+setOptimalOrder(mps::Mps, o::Vector{Int}) = (mps.optimalOrder[1:end]=o)
 
 function costPermutatingrecursive(curlistperm, array, product, cost, optimalcost, optimalsequence)
     if length(array)==1
@@ -52,6 +75,22 @@ function costPermutatingrecursive(curlistperm, array, product, cost, optimalcost
     end
 end
 
+
+
+
+*(mps::Mps, T::AbstractArray) = optimalmul2(mps, T)
+*(T::AbstractArray, mps::Mps) = optimalmul2(mps, T)
+
+ten(res, A, K1, K2, K3) = @tensor res[i,j,k] = A[u,v,w]*K1[i,u]*K2[j,v]*K3[k,w]
+*(A::Array{Float64,3}, K::Array{Array{Float64,2},1}) = ten(Array{Float64}(undef, size(A)), A, K[1], K[2],K[3])
+*(K::Array{Array{Float64,2},1}, A::Array{Float64,3}) = ten(Array{Float64}(undef, size(A)), A, K[1], K[2],K[3])
+
+#*(A::AbstractArray, K::Mps) = ten(Array{Float64}(undef, size(A)), A, K.matrices[1], K.matrices[2], K.matrices[3])
+#*( K::Mps, A::AbstractArray) = ten(Array{Float64}(undef, size(A)), A, K.matrices[1], K.matrices[2], K.matrices[3])
+
+
+"""
+
 function optimalmul(mps::Mps, T::AbstractArray)
     @assert last.(size.(mps.matrices)) == [s for s in size(T)]
 
@@ -74,7 +113,4 @@ function optimalmul(mps::Mps, T::AbstractArray)
     tensors[1:end-1]=mps.matrices
     ncon(tensors, network, order=mps.optimalOrder)
 end
-
-
-*(mps::Mps, T::AbstractArray) = optimalmul(mps, T)
-*(T::AbstractArray, mps::Mps) = optimalmul(mps, T)
+"""
