5935f003bb164cdcf45bb725f96a82c767cfa4b5
padwulf <pieter.dewulf@ugent.be>
2020-04-08 16:41:26 +0200

    correction R2 score

--- src/Scores.jl
+++ src/Scores.jl
@@ -1,8 +1,17 @@
 using ROC
+using Statistics
 
 auc_(scores,labels) = AUC(roc(scores, labels))
-R2_(scores, labels) = r2_score(labels, scores)
 
+function R2_(scores, labels)
+    dY = scores -labels
+    Ysc = labels - mean(labels)*ones(size(labels))
+    return 1 - (sum(dY.*dY) / sum(Ysc.*Ysc))
+end
+
+
+"""
+#R2_(scores, labels) = 1 - sum((scores-labels).*scores-labels)
 
 function explained_variance_score(y_true, y_pred)
     return(sum((y_pred .- mean(y_true)) .^ 2))
@@ -15,5 +24,7 @@ end
 function r2_score(y_true, y_pred)
     #println(y_true)
     #println(y_pred)
+    println((explained_variance_score(y_true, y_pred), total_variance_score(y_true, y_pred)))
     return(explained_variance_score(y_true, y_pred) / total_variance_score(y_true, y_pred))
 end
+"""
